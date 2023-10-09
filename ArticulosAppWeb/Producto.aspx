1fecbe9f8f27fffb6b6c57d3027418414fa35be8
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-10-09 11:59:27 -0300

    Implementé la pagina producto y su navegacion

---
+++ ArticulosAppWeb/Producto.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Producto.aspx.cs" Inherits="ArticulosAppWeb.Producto" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Content" runat="server">
+    <section>
+        <h2 id="TituloProducto" runat="server">Productos</h2>
+    </section>
+</asp:Content>
---
+++ ArticulosAppWeb/Producto.aspx.cs
@@ -0,0 +1,32 @@
+﻿using ArticulosAppModels;
+using ArticulosAppServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace ArticulosAppWeb
+{   
+    public partial class Producto : System.Web.UI.Page
+    {
+        public string ArticuloId;
+        Articulo ArticuloPagina;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ArticuloId = Request.QueryString["id"].ToString();
+                //TituloProducto.InnerText = "Producto " + ArticuloId;
+                ArticuloService articuloService = new ArticuloService();
+                ArticuloPagina = articuloService.GetAll().Find(x => x.Id == int.Parse(ArticuloId));
+            }
+            catch (Exception)
+            {
+                Response.Redirect("/");
+            }
+        }
+    }
+}
