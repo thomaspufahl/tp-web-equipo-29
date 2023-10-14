bb5d956e8e5281d26b0b8cc352a0deb93d943774
coutnico <nicoo_rodrii@hotmail.com>
2023-10-13 22:07:22 -0300

    Fix ddlUnidades al cambiar de imagenes

--- ArticulosAppWeb/Producto.aspx.cs
+++ ArticulosAppWeb/Producto.aspx.cs
@@ -11,51 +11,54 @@ using System.Web.UI.WebControls;
 
 
 namespace ArticulosAppWeb
-{   
+{
     public partial class Producto : System.Web.UI.Page
     {
         public string ArticuloId;
         public Articulo ArticuloPagina;
         protected void Page_Load(object sender, EventArgs e)
         {
-            try
+            if (!IsPostBack)
             {
-                ArticuloId = Request.QueryString["id"].ToString();
-                ArticuloPagina = ((Site)Master).ObtenerListaArticulos().Where(x => x.Id == int.Parse(ArticuloId)).FirstOrDefault();
-
-                NombreProducto.InnerText = ArticuloPagina.Nombre.ToString();
-                ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0).UrlImagen;
-                PrecioProducto.Text = "$ " +  ArticuloPagina.Precio.ToString();   
-                DescripcionArticulo.Text = ArticuloPagina.Descripcion.ToString();
-                lblCategoria.Text = ArticuloPagina.Categoria.ToString();
-                lblMarca.Text = ArticuloPagina.Marca.ToString();
-
                 ddlUnidades.Items.Add("1 Unidad");
                 ddlUnidades.Items.Add("2 Unidades");
                 ddlUnidades.Items.Add("3 Unidades");
                 ddlUnidades.Items.Add("4 Unidades");
                 ddlUnidades.Items.Add("5 Unidades");
                 ddlUnidades.Items.Add("Mas de 5 Unidades");
-
-                
             }
-            catch (Exception)
-            {
-                Response.Redirect("/");
-            }          
+                try
+                {
+                    ArticuloId = Request.QueryString["id"].ToString();
+                    ArticuloPagina = ((Site)Master).ObtenerListaArticulos().Where(x => x.Id == int.Parse(ArticuloId)).FirstOrDefault();
+
+                    NombreProducto.InnerText = ArticuloPagina.Nombre.ToString();
+                    ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0).UrlImagen;
+                    PrecioProducto.Text = "$ " + ArticuloPagina.Precio.ToString();
+                    DescripcionArticulo.Text = ArticuloPagina.Descripcion.ToString();
+                    lblCategoria.Text = ArticuloPagina.Categoria.ToString();
+                    lblMarca.Text = ArticuloPagina.Marca.ToString();
+
+                }
+                catch (Exception)
+                {
+                    Response.Redirect("/");
+                }
+
+            
         }
 
         protected void Siguiente_Click(object sender, EventArgs e)
         {
             try
             {
-            ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0 + 1).UrlImagen;
+                ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0 + 1).UrlImagen;
 
             }
             catch
             {
                 ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0).UrlImagen;
-             
+
             }
         }
 
@@ -67,10 +70,10 @@ namespace ArticulosAppWeb
                 ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0 - 1).UrlImagen;
 
             }
-            catch 
-            { 
+            catch
+            {
                 ImagenPrincipalArticulo.Src = ArticuloPagina.Imagenes.ElementAt(0).UrlImagen;
-               
+
             }
 
         }
