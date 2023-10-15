54482c76da83f6cc931610c121df4c9b4e6cbe2f
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-10-15 16:25:14 -0300

    Corregi detalles en html y css del Master

--- ArticulosAppWeb/Site.Master.designer.cs
+++ ArticulosAppWeb/Site.Master.designer.cs
@@ -49,14 +49,5 @@ namespace ArticulosAppWeb
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Content;
-
-        /// <summary>
-        /// FooterPredeterminado control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder FooterPredeterminado;
     }
 }
